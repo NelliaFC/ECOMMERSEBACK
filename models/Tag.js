db2dcddec5216ae97c419afcadf1f2a9a61cda22
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-05 14:49:20 -0600

    tag

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Tag extends Model {}
-
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    },
   },
   {
     sequelize,
@@ -16,5 +21,4 @@ Tag.init(
     modelName: 'tag',
   }
 );
-
 module.exports = Tag;
